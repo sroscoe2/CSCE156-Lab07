e193a80d3283b77657a71a61c6eda49c559f5ee4
Chris Bourke <cbourke@cse.unl.edu>
2019-10-15 21:16:07 -0500

    ported lab and handouts

---
+++ src/unl/cse/trucks/TruckList.java
@@ -0,0 +1,81 @@
+package unl.cse.trucks;
+
+public class TruckList {
+	
+	//TODO: add the head of your list here
+
+	/**
+	 * This function returns the size of the list, the number of
+	 * elements currently stored in it.
+	 * @return
+	 */
+	public int getSize() { 
+    	throw new UnsupportedOperationException("Not yet implemented.");
+	}
+
+	/**
+	 * This function clears out the contents of the list, making it an
+	 * empty list.
+	 */
+    public void clear() {
+    	throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    /**
+     * This method adds the given {@link Truck} instance to the beginning
+     * of the list.
+     * @param t
+     */
+    public void addToStart(Truck t) {
+    	throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    /**
+     * This method adds the given {@link Truck} instance to the end of
+     * the list.
+     * @param t
+     */
+    public void addToEnd(Truck t) {
+    	throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    /**
+     * This method removes the {@link Truck} from the given 
+     * <code>position</code>, indices start at 0.  Implicitly, the 
+     * remaining elements' indices are reduced.
+     * @param position
+     */
+    public void remove(int position) {
+    	throw new UnsupportedOperationException("Not yet implemented.");
+    }
+    
+    /**
+     * This is a private helper method that returns a {@link TruckListNode}
+     * corresponding to the given position.  Implementing this method
+     * is optional but may help you with other methods.
+     * @param position
+     * @return
+     */
+    private TruckListNode getTruckListNode(int position) {
+    	throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    /**
+     * Returns the {@link Truck} element stored at the given 
+     * <code>position</code>.
+     * @param position
+     * @return
+     */
+    public Truck getTruck(int position) {
+    	throw new UnsupportedOperationException("Not yet implemented.");    	
+    }
+
+    /**
+     * Prints this list to the standard output.
+     */
+    public void print() {
+    	throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+}
+
