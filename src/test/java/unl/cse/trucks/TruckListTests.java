f71a1b6a7464af74aea08e8e296b17a740f1bdd2
Hundter Biede <6586509+hbiede@users.noreply.github.com>
2020-03-30 11:34:22 -0500

    Add failure messages to JUnit assertions

--- src/test/java/unl/cse/trucks/TruckListTests.java
+++ src/test/java/unl/cse/trucks/TruckListTests.java
@@ -3,12 +3,12 @@ package unl.cse.trucks;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Random;
 
 /**
- * A JUnit test suite of test cases for the {@link TruckList} 
+ * A JUnit test suite of test cases for the {@link TruckList}
  * linked list implementation.
  *
  */
@@ -17,7 +17,7 @@ public class TruckListTests {
     private TruckList testList;
 
     /**
-     * Initializes a new {@link TruckList} 
+     * Initializes a new {@link TruckList}
      */
     @BeforeEach
     void setUp() {
@@ -25,37 +25,37 @@ public class TruckListTests {
     }
 
     /**
-     * Tests that the {@link TruckList#getSize()} is correct after 
-     * inserting a new {@link Truck} with the 
-     * {@link TruckList#addToStart(Truck)} method. 
+     * Tests that the {@link TruckList#getSize()} is correct after
+     * inserting a new {@link Truck} with the
+     * {@link TruckList#addToStart(Truck)} method.
      */
     @Test
     void getSizeAddToStart() {
         Truck testElement = Truck.createRandomTruck();
         for (int i = 0; i < 10; i++) {
-            assertEquals(i, testList.getSize());
+            assertEquals(i, testList.getSize(), String.format("Added %d elements to the start of the truck list and size() returned %d", i, testList.getSize()));
             testList.addToStart(testElement);
         }
     }
 
     /**
-     * Tests that the {@link TruckList#getSize()} is correct after 
-     * inserting a new {@link Truck} with the 
-     * {@link TruckList#addToEnd(Truck)} method. 
+     * Tests that the {@link TruckList#getSize()} is correct after
+     * inserting a new {@link Truck} with the
+     * {@link TruckList#addToEnd(Truck)} method.
      */
     @Test
     void getSizeAddToEnd() {
         Truck testElement = Truck.createRandomTruck();
         for (int i = 0; i < 10; i++) {
-            assertEquals(i, testList.getSize());
+            assertEquals(i, testList.getSize(), String.format("Added %d elements to the end of the truck list and size() returned %d", i, testList.getSize()));
             testList.addToEnd(testElement);
         }
     }
 
     /**
-     * Tests that the {@link TruckList#getSize()} is correct after 
-     * removing a {@link Truck} with the {@link TruckList#remove(int)} 
-     * method. 
+     * Tests that the {@link TruckList#getSize()} is correct after
+     * removing a {@link Truck} with the {@link TruckList#remove(int)}
+     * method.
      */
     @Test
     void getSizeRemove() {
@@ -66,51 +66,53 @@ public class TruckListTests {
 
         Random randomRemover = new Random();
         for (int i = 10; i > 0; i--) {
-            assertEquals(i, testList.getSize());
+            assertEquals(i, testList.getSize(), String.format("Removed elements from a truck list leaving %d elements and size() returned %d", i, testList.getSize()));
             testList.remove(randomRemover.nextInt(i));
         }
     }
 
     /**
-     * Tests that the {@link TruckList#getSize()} is correct after 
-     * clearing out the list using the {@link TruckList#clear()} 
-     * method. 
+     * Tests that the {@link TruckList#getSize()} is correct after
+     * clearing out the list using the {@link TruckList#clear()}
+     * method.
      */
     @Test
     void clear() {
-        assertEquals(0, testList.getSize());
+        assertEquals(0, testList.getSize(), "Lists should start empty");
         testList.clear();
-        assertEquals(0, testList.getSize());
+        assertEquals(0, testList.getSize(), "Clearing should empty a list");
         testList.addToEnd(Truck.createRandomTruck());
         testList.addToStart(Truck.createRandomTruck());
+        testList.clear();
+        assertEquals(0, testList.getSize(), "Clearing should empty a list");
     }
 
     /**
-     * Tests that the {@link TruckList#addToStart(Truck)} is correct. 
+     * Tests that the {@link TruckList#addToStart(Truck)} is correct.
      */
     @Test
     void addToStart() {
         for (int i = 0; i < 10; i++) {
             Truck testElement = Truck.createRandomTruck();
             testList.addToStart(testElement);
-            assertEquals(testElement, testList.getTruck(0));
+            assertEquals(testElement, testList.getTruck(0), String.format("Added %d elements to the start of the truck list and size() returned %d", i, testList.getSize()));
         }
     }
 
     /**
-     * Tests that the {@link TruckList#addToEnd(Truck)} is correct. 
+     * Tests that the {@link TruckList#addToEnd(Truck)} is correct.
      */
     @Test
     void addToEnd() {
         for (int i = 0; i < 10; i++) {
             Truck testElement = Truck.createRandomTruck();
             testList.addToEnd(testElement);
-            assertEquals(testElement, testList.getTruck(i));
+            assertEquals(testElement, testList.getTruck(i), String.format("Added %d elements to the end of the truck list and size() returned %d", i, testList.getSize()));
         }
     }
 
     /**
-     * Tests that the {@link TruckList#remove(int)} is correct. 
+     * Tests that the {@link TruckList#remove(int)} is correct.
      */
     @Test
     void remove() {
@@ -121,41 +123,41 @@ public class TruckListTests {
             testList.addToEnd(t);
         }
 
-        assertEquals(testElements[0], testList.getTruck(0));
-        assertEquals(testElements[1], testList.getTruck(1));
-        assertEquals(testElements[2], testList.getTruck(2));
-        assertEquals(testElements[3], testList.getTruck(3));
-        assertEquals(testElements[4], testList.getTruck(4));
-        assertEquals(5, testList.getSize());
+        assertEquals(testElements[0], testList.getTruck(0), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(testElements[1], testList.getTruck(1), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(testElements[2], testList.getTruck(2), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(testElements[3], testList.getTruck(3), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(testElements[4], testList.getTruck(4), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(5, testList.getSize(), String.format("size() returned %d for a list with %d elements", testList.getSize(), 5));
 
         testList.remove(0);
-        assertEquals(testElements[1], testList.getTruck(0));
-        assertEquals(testElements[2], testList.getTruck(1));
-        assertEquals(testElements[3], testList.getTruck(2));
-        assertEquals(testElements[4], testList.getTruck(3));
-        assertEquals(4, testList.getSize());
+        assertEquals(testElements[1], testList.getTruck(0), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(testElements[2], testList.getTruck(1), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(testElements[3], testList.getTruck(2), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(testElements[4], testList.getTruck(3), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(4, testList.getSize(), String.format("size() returned %d for a list with %d elements", testList.getSize(), 4));
 
         testList.remove(3);
-        assertEquals(testElements[1], testList.getTruck(0));
-        assertEquals(testElements[2], testList.getTruck(1));
-        assertEquals(testElements[3], testList.getTruck(2));
-        assertEquals(3, testList.getSize());
+        assertEquals(testElements[1], testList.getTruck(0), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(testElements[2], testList.getTruck(1), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(testElements[3], testList.getTruck(2), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(3, testList.getSize(), String.format("size() returned %d for a list with %d elements", testList.getSize(), 3));
 
         testList.remove(1);
-        assertEquals(testElements[1], testList.getTruck(0));
-        assertEquals(testElements[3], testList.getTruck(1));
-        assertEquals(2, testList.getSize());
+        assertEquals(testElements[1], testList.getTruck(0), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(testElements[3], testList.getTruck(1), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(2, testList.getSize(), String.format("size() returned %d for a list with %d elements", testList.getSize(), 2));
 
         testList.remove(0);
-        assertEquals(testElements[3], testList.getTruck(0));
-        assertEquals(1, testList.getSize());
+        assertEquals(testElements[3], testList.getTruck(0), "remove() is causing trucks to be in an unexpected order");
+        assertEquals(1, testList.getSize(), String.format("size() returned %d for a list with %d elements", testList.getSize(), 1));
 
         testList.remove(0);
-        assertEquals(0, testList.getSize());
+        assertEquals(0, testList.getSize(), String.format("size() returned %d for a list with %d elements", testList.getSize(), 0));
     }
 
     /**
-     * Tests that the {@link TruckList#getTruck(int)} is correct. 
+     * Tests that the {@link TruckList#getTruck(int)} is correct.
      */
     @Test
     void getTruck() {
@@ -166,11 +168,11 @@ public class TruckListTests {
             testList.addToEnd(t);
         }
 
-        assertEquals(testElements[0], testList.getTruck(0));
-        assertEquals(testElements[1], testList.getTruck(1));
-        assertEquals(testElements[2], testList.getTruck(2));
-        assertEquals(testElements[3], testList.getTruck(3));
-        assertEquals(testElements[4], testList.getTruck(4));
+        assertEquals(testElements[0], testList.getTruck(0), "getTruck() is returning an unexpected element");
+        assertEquals(testElements[1], testList.getTruck(1), "getTruck() is returning an unexpected element");
+        assertEquals(testElements[2], testList.getTruck(2), "getTruck() is returning an unexpected element");
+        assertEquals(testElements[3], testList.getTruck(3), "getTruck() is returning an unexpected element");
+        assertEquals(testElements[4], testList.getTruck(4), "getTruck() is returning an unexpected element");
     }
 
 
