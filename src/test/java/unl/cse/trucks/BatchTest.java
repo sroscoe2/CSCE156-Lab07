27adc1008a8082c3c385a3178200bdb40fdf2213
Chris Bourke <cbourke@cse-cbourke-06.unl.edu>
2021-04-08 13:26:15 -0500

    modified to use junit 5 exclusive custom launcher.
    Previous solutions used a combination of junit 4 and 5 aspects which
    were ultimately incompatible.  The Test annotation that used junit 4
    would not respect other annotations such as BeforeEach while switching
    to junit 5 meant the old BatchTester could not discover any tests.  This
    version uses junit 5 exclusively.

--- src/test/java/unl/cse/trucks/BatchTest.java
+++ src/test/java/unl/cse/trucks/BatchTest.java
@@ -1,30 +1,59 @@
 package unl.cse.trucks;
 
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.*;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
+
+import java.io.PrintWriter;
 
 /**
- * This is a batch test file used by grading  
- * scripts to generate a full roster grade report.
+ * This is a batch test file used by grading scripts to generate a full roster
+ * grade report.
  * 
  * @author cbourke
  *
  */
 public class BatchTest {
 
+	SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
+	public void runAll() {
+		LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+				.selectors(selectClass(TruckListTests.class))
+				.build();
+		Launcher launcher = LauncherFactory.create();
+		TestPlan testPlan = launcher.discover(request);
+		launcher.registerTestExecutionListeners(listener);
+		launcher.execute(request);
+	}
+
 	public static void main(String[] args) {
 
-		int labPoints = 20;
-		JUnitCore jUnitCore = new JUnitCore();
-		Result r = jUnitCore.run(TruckListTests.class);
+		BatchTest bt = new BatchTest();
 
-		// prints total number of points, number of pass/fail 
+		bt.runAll();
+
+		TestExecutionSummary summary = bt.listener.getSummary();
+		// summary.printTo(new PrintWriter(System.out));
+
+		int labPoints = 20;
+		long numTests = summary.getTestsFoundCount();
+		long numFail = summary.getTestsFailedCount();
+		long numPass = summary.getTestsSucceededCount();
+
+		// prints total number of points, number of pass/fail
 		// and total tests in csv format
 		System.out.printf("%d,%d,%d,%d", 
-				r.getFailureCount() == 0 ? labPoints : 0,
-				r.getRunCount() - r.getFailureCount(), 
-				r.getFailureCount(), 
-				r.getRunCount());
+				        numFail == 0 ? labPoints : 0, 
+						numPass, 
+						numFail, 
+						numTests);
+
 	}
 
 }
